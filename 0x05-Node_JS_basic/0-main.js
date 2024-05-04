3fbfef40953ca204182c09e9ca3af99ea89f77ff
seedumar1999 <seedumar1999@gmail.com>
2024-05-04 11:49:59 -0700

    Update

---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,5 @@
+
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
+
---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+
+class AppController {
+    static getHomepage(request, response) {
+      response.send(200, 'Hello Holberton School!');
+    }
+  }
+  
+  export default AppController;
+
